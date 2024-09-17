9095b67f2f45fa706251cf9e5fa55ab09e8458b9
JORGE CASTRO GUTIÉRREZ <jorgecas71@MacBook-Air-de-JORGE.local>
2024-09-17 15:41:31 +0200

    added changes in search styles

--- src/components/SearchBuy.vue
+++ src/components/SearchBuy.vue
@@ -2,29 +2,116 @@
 import { ref } from 'vue';
 import SearchService from '../core/SearchService.js';
 import SearchServiceRepository from '../core/SearchRepository.js';
-
 const query = ref('');
 const filteredProperties = ref([]);
-
+const isCompraSelected = ref(false);
 const repository = new SearchServiceRepository();
 const searchService = new SearchService(repository);
-
 const searchProperties = () => {
     filteredProperties.value = searchService.search(query.value);
 };
+const selectCompra = () => {
+    isCompraSelected.value = true;
+};
 </script>
-
 <template>
-    <div>
-        <input v-model="query" @input="searchProperties" placeholder="Buscar propiedades..." />
-        <ul>
-            <li v-for="property in filteredProperties" :key="property.id">
-                {{ property.name }} - {{ property.price }}
-            </li>
-        </ul>
+    <div class="container">
+        <div class="search-bar d-flex justify-content-between align-items-center p-3 ">
+            <button class="btn btn-primary" @click="selectCompra"
+                :class="{ 'selected': isCompraSelected }">Compra</button>
+            <input v-model="query" @input="searchProperties" class="form-control mx-3"
+                placeholder="Buscar propiedades..." />
+            <button class="btn btn-secondary">Buscar</button>
+        </div>
     </div>
 </template>
+<style>
+.container {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding-top: 500px;
+}
 
-<style scoped>
+.search-bar {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 140vh;
+    height: 10vh;
+    background-color: transparent;
+    padding: 20px;
+    border-radius: 10px;
+    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+    position: absolute;
+    opacity: 1;
 
+}
+
+input {
+    width: 120vh;
+    height: 90px;
+    font-size: 18px;
+    background-color: #D9D9D9;
+    border: 1px solid #650000;
+    box-shadow: 0 2px 4px rgba(1, 1, 1, 0.1);
+}
+
+.btn-primary {
+    background-color: #D9D9D9;
+    border-color: #D9D9D9;
+    padding: 32px 20px;
+    font-size: 18px;
+    box-shadow: 0 2px 4px rgba(1, 1, 1, 0.1);
+}
+
+.btn-primary:hover {
+    background-color: #FFFFFF;
+    border-color: #650000;   
+
+}
+
+.btn-primary.selected {
+    background-color: #650000;
+    border-color: #fff;
+    color: white;
+}
+
+.btn-secondary {
+    background-color: #D9D9D9;
+    border-color: #D9D9D9;
+    padding: 32px 20px;
+    font-size: 18px;
+    box-shadow: 0 2px 4px rgba(1, 1, 1, 0.1);
+}
+
+.btn-secondary:hover {
+    background-color: #FFFFFF;
+    border-color: #650000;
+    color: #650000;
+
+}
+
+.form-control {
+    flex-grow: 1;
+    border: 1px solid #D9D9D9;
+    padding: 20px;
+    padding-right: 550px;
+}
+
+.list-group-item {
+    border: 1px solid #D9D9D9;
+
+    margin-bottom: 5px;
+}
 </style>
+
+
+
+
+
+
+
+
+
+
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,11 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+
+import SearchBuy from '@/components/SearchBuy.vue';
+
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+
+      <SearchBuy />
+  
 </template>
