cf0941aa0ecf2e833787ca2817c8789ed68e17ea
JORGE CASTRO GUTIÉRREZ <jorgecas71@MacBook-Air-de-JORGE.local>
2024-09-18 09:28:47 +0200

    added search and image

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,7 +5,32 @@ import SearchBuy from '@/components/SearchBuy.vue';
 </script>
 
 <template>
+      <div class="text">
+            <h1>¡No necesitas de una bici voladora para encontrar tu hogar</h1>
+            <h1>Descubre tu casa ideal en Asturias.</h1>
+      </div>
 
       <SearchBuy />
-  
+
 </template>
+<style >
+body {
+      background-image: url('../assets/images/portada.jpg');
+      background-size: cover;
+}
+
+.text {
+font-family: "Jomolhari", serif;
+      color: #650000;
+      font-size: 2vh;
+      text-shadow:
+            1px 1px 2px rgba(0, 0, 0, 0.5),
+            -1px 1px 2px rgba(0, 0, 0, 0.5),
+            1px -1px 2px rgba(0, 0, 0, 0.5),
+            -1px -1px 2px rgba(0, 0, 0, 0.5);
+      text-align: center;
+    
+}
+
+
+</style>
