bf5d2c1d19b79d1ee2a9e86ce36476b9587b76f4
Julia <musicaparatodo770@gmail.com>
2024-09-27 08:53:11 +0200

    added type button in search filter

--- src/core/models/Property.js
+++ src/core/models/Property.js
@@ -1,11 +1,18 @@
 
+
+
 export default class Property {
-    constructor(id, price, description, address, area, action) {
+    constructor(id, price, description, address, area, action, bedrooms, elevator, floor, bathrooms, house) {
         this.id = id;
         this.price = price;
         this.description = description;
         this.address = address;
         this.area = area;
-        this.action = action; 
+        this.action = action;
+        this.bedrooms = bedrooms; 
+        this.elevator = elevator;
+        this.floor = floor;
+        this.bathrooms = bathrooms;
+        this.house = house;
     }
 }
