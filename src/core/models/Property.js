15615848b2691ad776be39b35fed6a229fc0dd83
Antonella <168837149+AntonellaEL@users.noreply.github.com>
2024-10-03 15:16:12 +0200

    fixed card information

--- src/core/models/Property.js
+++ src/core/models/Property.js
@@ -1,18 +1,15 @@
-
-
-
 export default class Property {
-    constructor(id, price, description, address, area, action, bedrooms, elevator, floor, bathrooms, house) {
+    constructor(id, price, description, address, area, action, room, hasElevator, floors, bathroom, house) {
         this.id = id;
         this.price = price;
         this.description = description;
         this.address = address;
         this.area = area;
         this.action = action;
-        this.bedrooms = bedrooms; 
-        this.elevator = elevator;
-        this.floor = floor;
-        this.bathrooms = bathrooms;
+        this.room = room; 
+        this.hasElevator = hasElevator;
+        this.floors = floors;
+        this.bathroom = bathroom;
         this.house = house;
     }
 }
